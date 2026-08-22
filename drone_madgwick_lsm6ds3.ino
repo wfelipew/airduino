a79e02e893e354a30e1604708ade3ef8e2722aca
William Welter <wfelipew@gmail.com>
2026-08-22 19:40:24 +0100

    Drone stable

--- drone_madgwick_lsm6ds3.ino
+++ drone_madgwick_lsm6ds3.ino
@@ -26,7 +26,8 @@ Wire.begin(); -> // Wire.begin();
 // #define IMU_BOARD MPU6050_BOARD
 
 
-#include <Servo.h>
+// #include <Servo.h>
+#include <SAMD_PWM.h>
 #include <ServoInput.h>
 #include <Wire.h>
 #include <SPI.h>
@@ -82,42 +83,54 @@ Evaluate list:
 #define pinESC3 9
 #define pinESC4 10
 #define pinButton 3
-#define pinPot A2
+#define pinBattery A2
 
 #define testSpeed 0
 
 #define PID_ANGLE_AMP 3
 #define PID_MIN_SPEED_THRESHOLD 1100
-#define PID_TAKEOFF_THRESHOLD 1400
+#define PID_TAKEOFF_THRESHOLD 1200
 #define MAX_RATE_SETPOINT_DPS 164
 
-#define SPEED_LIMIT_RAW 1600
-#define SPEED_MAX_OUTPUT 1700
+#define SPEED_LIMIT_RAW 1800
+#define SPEED_MAX_OUTPUT 1900
 #define SPEED_MIN_OUTPUT 1010
 
 #define LEVEL_FILTER_ALPHA 0.05
 #define LEVEL_GAIN_PITCH 3.0
 #define LEVEL_GAIN_ROLL  3.0
 
-#define PID_P_GAIN_PITCH 1 //0.8
-#define PID_I_GAIN_PITCH 0.1 //0.004
-#define PID_D_GAIN_PITCH 12//15 8 12
+#define PID_P_GAIN_PITCH 1.3 //0.8
+#define PID_I_GAIN_PITCH 0.04 //0.004
+#define PID_D_GAIN_PITCH 18//15 8 12
 
-#define PID_P_GAIN_ROLL 1 //0.8
-#define PID_I_GAIN_ROLL 0.1//0.004 // 0.002 was ok , por regra de 3 deveria se 0.012 <- testar
-#define PID_D_GAIN_ROLL 12//15 8 12
+#define PID_P_GAIN_ROLL 1.3 //0.8
+#define PID_I_GAIN_ROLL 0.04 //0.004 // 0.002 was ok , por regra de 3 deveria se 0.012 <- testar
+#define PID_D_GAIN_ROLL 18//15 8 12
 
 #define PID_P_GAIN_YAW 3 // 1
 #define PID_I_GAIN_YAW 0.01 //0.02//0.002//0.02
 #define PID_D_GAIN_YAW 0
 
 
+#define PID_I_MAX 350 //150
+#define PID_I_YAW_MAX 100 //150
+#define PID_YAW_MAX 250 //150
+
+#define REFERENCE_VOLTAGE  3.3    // Nano 33 IoT logic level
+#define BATTERY_R1  10000.0
+#define BATTERY_R2  2000.0
+
 Madgwick filter;
 
-Servo esc1;
-Servo esc2;
-Servo esc3;
-Servo esc4;
+// Servo esc1;
+// Servo esc2;
+// Servo esc3;
+// Servo esc4;
+SAMD_PWM* esc1;
+SAMD_PWM* esc2;
+SAMD_PWM* esc3;
+SAMD_PWM* esc4;
 
 const int SDA_PIN = A4; 
 const int SCL_PIN = A5;
@@ -125,6 +138,11 @@ const int SCL_PIN = A5;
 int engineSpeed = 1000;
 int engineSpeed_raw = 1000;
 int previous_engineSpeed_raw = 1000;
+
+int batteryADCRaw = 0;
+float batteryADC = 0;
+float batteryVin = 0;
+float batteryVinRaw = 0;
 
 // MPU control/status vars
 bool dmpReady = false;  // set true if DMP init was successful
@@ -149,23 +167,48 @@ float gyro_offset[3];
 int16_t gyro_internal_offset[3];
 float accel_offset[3];
 
-
+struct log {
+  float gyro_pitch;
+  float gyro_roll;
+  float gyro_yaw;
+  float angle_pitch;
+  float angle_roll;
+  float angle_yaw;
+  int rc_engine_speed;
+  float rc_pitch;
+  float rc_roll;
+  float rc_yaw;
+  float error_pitch;
+  float error_roll;
+  float error_yaw;
+  float i_pitch;
+  float i_roll;
+  float i_yaw;
+  int leftRear;
+  int rightRear;
+  int leftFront;
+  int rightFront;
+  float batteryVin;
+} flightLog;
 
 // Timers
 unsigned long loop_timer;
+unsigned long log_timer;
 unsigned long lastGoodPacket = 0;
 
 // Status
 volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
 
 // Control
-float pitchAngle = 0, rollAngle=0;
-float pitch_level_adjust = 0, roll_level_adjust = 0;
-long pitchAngle_raw = 0, rollAngle_raw = 0;
-long previous_pitchAngle_raw = 0, previous_rollAngle_raw = 0;
+float pitchAngle = 0, rollAngle=0, yawAngle=0;
+float pitch_level_adjust = 0, roll_level_adjust = 0, yaw_level_adjust = 0;
+long pitchAngle_raw = 0, rollAngle_raw = 0, yawAngle_raw = 0 ;
+long previous_pitchAngle_raw = 0, previous_rollAngle_raw = 0, previous_yawAngle_raw = 0 ;
 ServoInputPin<3> rf_throttle;
 ServoInputPin<A1> rf_pitch;
 ServoInputPin<A7> rf_roll;
+ServoInputPin<11> rf_yaw;
+
 
 // PID
 float pid_p_roll, pid_i_roll, pid_d_roll, pid_roll_output, roll_error, roll_error_previous;
@@ -181,26 +224,39 @@ File fdr_file;
 void setup() {
 
   // The first thing to do is attach to the esc's
-  esc1.attach(pinESC1,1000,2000);
-  esc2.attach(pinESC2,1000,2000);
-  esc3.attach(pinESC3,1000,2000);
-  esc4.attach(pinESC4,1000,2000);
+  // esc1.attach(pinESC1,1000,2000);
+  // esc2.attach(pinESC2,1000,2000);
+  // esc3.attach(pinESC3,1000,2000);
+  // esc4.attach(pinESC4,1000,2000);
+  esc1 = new SAMD_PWM(pinESC1, 250.0f, 25.0f);
+  esc2 = new SAMD_PWM(pinESC2, 250.0f, 25.0f);
+  esc3 = new SAMD_PWM(pinESC3, 250.0f, 25.0f);
+  esc4 = new SAMD_PWM(pinESC4, 250.0f, 25.0f);
 
   // And then set it the esc to 0 to arm the engines
-  esc1.write(0);
-  esc2.write(0);
-  esc3.write(0);
-  esc4.write(0);
+  // esc1.write(1000);
+  // esc2.write(1000);
+  // esc3.write(1000);
+  // esc4.write(1000);
+  esc1->setPWM(pinESC1,250.0f,1000/40.0f);
+  esc2->setPWM(pinESC2,250.0f,1000/40.0f);
+  esc3->setPWM(pinESC3,250.0f,1000/40.0f);
+  esc4->setPWM(pinESC4,250.0f,1000/40.0f);
 
   // Attach the servo inputs
   rf_throttle.attach();
   rf_pitch.attach();
   rf_roll.attach();
+  rf_yaw.attach();
 
   // Start serial communication
   Serial.begin(230400);//115200
   // delay(8000);
   Serial.println("Starting setup");
+
+  // Battery stuff
+  analogReadResolution(12);
+  batteryADC = (float)analogRead(pinBattery);
  
 
   // Start I2C communication
@@ -328,9 +384,9 @@ void loop() {
   // gx_dps *=-1;
 
   if( ! (abs(gx_dps) > 400 || abs(gy_dps) > 400 || abs(gz_dps) > 400)  ){
-    gyro_filtered[0]= ( (0.7 * gyro_filtered[0])  + (0.5 * gx_dps) );
-    gyro_filtered[1]= ( (0.7 * gyro_filtered[1])  + (0.5 * gy_dps) );
-    gyro_filtered[2]= ( (0.7 * gyro_filtered[2])  + (0.5 * gz_dps) );
+    gyro_filtered[0]= ( (0.7 * gyro_filtered[0])  + (0.3 * gx_dps) );
+    gyro_filtered[1]= ( (0.7 * gyro_filtered[1])  + (0.3 * gy_dps) );
+    gyro_filtered[2]= ( (0.7 * gyro_filtered[2])  + (0.3 * gz_dps) );
 
   }
   lastGoodPacket = millis();
@@ -352,23 +408,31 @@ void loop() {
   last_az_raw = az_raw;
 
   #ifdef PRINT_PARAMETERS
-    buf += String(gyro_filtered[1] );
-    buf += F(",");
+    flightLog.gyro_pitch = gyro_filtered[1];
+    flightLog.gyro_roll = gyro_filtered[0];
+    flightLog.gyro_yaw = gyro_filtered[2];
 
-    buf += String(gyro_filtered[0] );
-    buf += F(",");
+    flightLog.angle_pitch = ypr[1];
+    flightLog.angle_roll = ypr[0];
+    flightLog.angle_yaw = ypr[2];
 
-    buf += String(gyro_filtered[2] );
-    buf += F(",");
+    // buf += String(gyro_filtered[1] );
+    // buf += F(",");
 
-    buf += String(ypr[1]);
-    buf += F(",");
+    // buf += String(gyro_filtered[0] );
+    // buf += F(",");
 
-    buf += String(ypr[2]);
-    buf += F(",");
+    // buf += String(gyro_filtered[2] );
+    // buf += F(",");
+
+    // buf += String(ypr[1]);
+    // buf += F(",");
+
+    // buf += String(ypr[2]);
+    // buf += F(",");
     
-    buf += String(ypr[0]);
-    buf += F(",");
+    // buf += String(ypr[0]);
+    // buf += F(",");
   #endif
 
   engineSpeed_raw = rf_throttle.getPulse();
@@ -379,6 +443,7 @@ void loop() {
   engineSpeed = (0.8 * engineSpeed) +  (0.2 * engineSpeed_raw);
   pitchAngle_raw = rf_pitch.mapDeadzone(-MAX_RATE_SETPOINT_DPS, MAX_RATE_SETPOINT_DPS, 0.1) *-1;
   rollAngle_raw = rf_roll.mapDeadzone(-MAX_RATE_SETPOINT_DPS, MAX_RATE_SETPOINT_DPS, 0.1);
+  yawAngle_raw = rf_yaw.mapDeadzone(-MAX_RATE_SETPOINT_DPS, MAX_RATE_SETPOINT_DPS, 0.1);
 
   if(abs(pitchAngle_raw - previous_pitchAngle_raw) > 60){
     pitchAngle_raw = previous_pitchAngle_raw;
@@ -388,12 +453,20 @@ void loop() {
     rollAngle_raw = previous_rollAngle_raw;
   }
 
+  if(abs(yawAngle_raw - previous_yawAngle_raw) > 60){
+    yawAngle_raw = previous_yawAngle_raw;
+  }
+
   pitchAngle = (0.7 * pitchAngle) + (0.3 * pitchAngle_raw);
   rollAngle = (0.7 * rollAngle) + (0.3 * rollAngle_raw);
+  yawAngle = (0.7 * yawAngle) + (0.3 * yawAngle_raw);
 
   previous_pitchAngle_raw = pitchAngle_raw;
   previous_rollAngle_raw = rollAngle_raw;
+  previous_yawAngle_raw = yawAngle_raw;
   previous_engineSpeed_raw = engineSpeed_raw;
+
+  readBatteryVoltage();
   
   if(engineSpeed<1040){
     disableEngines();
@@ -404,20 +477,43 @@ void loop() {
     enableEngines();
   }
   #ifdef PRINT_PARAMETERS
-    buf += String(engineSpeed);
-    buf += F(",");
+    flightLog.rc_engine_speed = engineSpeed;
+    flightLog.rc_pitch = pitchAngle;
+    flightLog.rc_roll = rollAngle;
+    flightLog.rc_yaw = yawAngle;
 
-    buf += String(pitchAngle);
-    buf += F(",");
+    flightLog.batteryVin = batteryVin;
 
-    buf += String(rollAngle);
-    buf += F(",");
+    // buf += String(engineSpeed);
+    // buf += F(",");
+
+    // buf += String(pitchAngle);
+    // buf += F(",");
+
+    // buf += String(rollAngle);
+    // buf += F(",");
   #endif
 
   setAllEnginesSpeed(engineSpeed);
   #ifdef PRINT_PARAMETERS
-    Serial.print(buf);
-    Serial.print("\n");
+    // Serial.print(buf);
+    if(micros() - log_timer > 100000 ){
+      char buffer[100];
+      sprintf(buffer,"%f,%f,%f,%f,%f,%f,%d,%f,%f,%f,%f", flightLog.gyro_pitch,
+                                                flightLog.gyro_roll,
+                                                flightLog.gyro_yaw,
+                                                flightLog.angle_pitch,
+                                                flightLog.angle_roll,
+                                                flightLog.angle_yaw,
+                                                flightLog.rc_engine_speed,
+                                                flightLog.rc_pitch,
+                                                flightLog.rc_roll,
+                                                flightLog.rc_yaw,
+                                                flightLog.batteryVin);
+      Serial.println(buffer);
+      log_timer = micros();
+    }
+    
   #endif
 
   while(micros() - loop_timer < 4000 );                                      //We wait until 4000us are passed. // VOLTAR
@@ -427,6 +523,7 @@ void loop() {
     Serial.print("\n");
   #endif
   loop_timer = micros();
+  
 
   // digitalWrite(LED_BUILTIN, LOW);
   
@@ -436,10 +533,14 @@ void loop() {
 }
 
 void disableEngines(){
-  esc1.writeMicroseconds(1000);
-  esc2.writeMicroseconds(1000);
-  esc3.writeMicroseconds(1000);
-  esc4.writeMicroseconds(1000);
+  // esc1.writeMicroseconds(1000);
+  // esc2.writeMicroseconds(1000);
+  // esc3.writeMicroseconds(1000);
+  // esc4.writeMicroseconds(1000);
+  esc1->setPWM(pinESC1,250.0f,1000/40.0f);
+  esc2->setPWM(pinESC2,250.0f,1000/40.0f);
+  esc3->setPWM(pinESC3,250.0f,1000/40.0f);
+  esc4->setPWM(pinESC4,250.0f,1000/40.0f);
   
   //fdr_file.flush();
   
@@ -451,21 +552,21 @@ void disableEngines(){
 
 void enableEngines(){
  
-  if(!esc1.attached()){
-    esc1.attach(pinESC1,1000,2000);
-  }
+  // if(!esc1.attached()){
+  //   esc1.attach(pinESC1,1000,2000);
+  // }
 
-  if(!esc2.attached()){
-    esc2.attach(pinESC2,1000,2000);
-  }
+  // if(!esc2.attached()){
+  //   esc2.attach(pinESC2,1000,2000);
+  // }
 
-  if(!esc3.attached()){
-    esc3.attach(pinESC3,1000,2000);
-  }
+  // if(!esc3.attached()){
+  //   esc3.attach(pinESC3,1000,2000);
+  // }
 
-  if(!esc4.attached()){
-    esc4.attach(pinESC4,1000,2000);
-  }  
+  // if(!esc4.attached()){
+  //   esc4.attach(pinESC4,1000,2000);
+  // }  
 }
 
 
@@ -483,13 +584,13 @@ void setAllEnginesSpeed(int speed){
   // pitch_gyro_desired = constrain(pitchAngle - pitch_level_adjust, -MAX_RATE_SETPOINT_DPS, MAX_RATE_SETPOINT_DPS);
   // roll_gyro_desired  = constrain(rollAngle  - roll_level_adjust,  -MAX_RATE_SETPOINT_DPS, MAX_RATE_SETPOINT_DPS);
   
-  pitch_gyro_desired = constrain(( pitchAngle - ((ypr[1] * 3.0 ))),-MAX_RATE_SETPOINT_DPS, MAX_RATE_SETPOINT_DPS);
-  roll_gyro_desired =  constrain(( rollAngle - ((ypr[2] * 3.0 ))),-MAX_RATE_SETPOINT_DPS, MAX_RATE_SETPOINT_DPS);
+  pitch_gyro_desired = constrain(( pitchAngle - ((ypr[1] * 5.0 ))),-MAX_RATE_SETPOINT_DPS, MAX_RATE_SETPOINT_DPS);
+  roll_gyro_desired =  constrain(( rollAngle - ((ypr[2] * 5.0 ))),-MAX_RATE_SETPOINT_DPS, MAX_RATE_SETPOINT_DPS);
 
   // pitch_gyro_desired = pitchAngle ;
   // roll_gyro_desired =  rollAngle ;
 
-  yaw_gyro_desired = 0;
+  yaw_gyro_desired = yawAngle;
 
   pitch_error = pitch_gyro_desired - gyro_filtered[1];
   roll_error  = roll_gyro_desired - gyro_filtered[0];
@@ -508,14 +609,17 @@ void setAllEnginesSpeed(int speed){
   // }
 
   #ifdef PRINT_PARAMETERS
-    buf += String(pitch_error);
-    buf += F(",");
+    flightLog.error_pitch = pitch_error;
+    flightLog.error_roll = roll_error;
+    flightLog.error_yaw = yaw_error;
+    // buf += String(pitch_error);
+    // buf += F(",");
 
-    buf += String(roll_error,3);
-    buf += F(",");    
+    // buf += String(roll_error,3);
+    // buf += F(",");    
 
-    buf += String(yaw_error,3);
-    buf += F(",");
+    // buf += String(yaw_error,3);
+    // buf += F(",");
   #endif
 
   if(speed >= PID_MIN_SPEED_THRESHOLD){
@@ -524,10 +628,10 @@ void setAllEnginesSpeed(int speed){
     pid_p_pitch  = pitch_error * PID_P_GAIN_PITCH;
     if( speed > PID_TAKEOFF_THRESHOLD){
       pid_i_pitch  += pitch_error * PID_I_GAIN_PITCH;
-      if (pid_i_pitch > pid_max) {
-          pid_i_pitch = pid_max;      
-      }else if(pid_i_pitch < pid_max * -1){
-          pid_i_pitch = pid_max * -1;
+      if (pid_i_pitch > PID_I_MAX) {
+          pid_i_pitch = PID_I_MAX;      
+      }else if(pid_i_pitch < PID_I_MAX * -1){
+          pid_i_pitch = PID_I_MAX * -1;
       }
     }else{
       pid_i_pitch = 0;
@@ -546,10 +650,10 @@ void setAllEnginesSpeed(int speed){
     pid_p_roll = roll_error * PID_P_GAIN_ROLL;
     if( speed > PID_TAKEOFF_THRESHOLD){
       pid_i_roll += roll_error * PID_I_GAIN_ROLL;
-      if (pid_i_roll > pid_max) {
-          pid_i_roll = pid_max;      
-      }else if(pid_i_roll < pid_max * -1){
-          pid_i_roll = pid_max * -1;
+      if (pid_i_roll > PID_I_MAX) {
+          pid_i_roll = PID_I_MAX;      
+      }else if(pid_i_roll < PID_I_MAX * -1){
+          pid_i_roll = PID_I_MAX * -1;
       }
     }else{
       pid_i_roll = 0;
@@ -570,10 +674,10 @@ void setAllEnginesSpeed(int speed){
     pid_p_yaw = yaw_error * PID_P_GAIN_YAW;
     if( speed > PID_TAKEOFF_THRESHOLD){
       pid_i_yaw += yaw_error * PID_I_GAIN_YAW; 
-      if (pid_i_yaw > pid_max) {
-          pid_i_yaw = pid_max;      
-      }else if(pid_i_yaw < pid_max * -1){
-          pid_i_yaw = pid_max * -1;
+      if (pid_i_yaw > PID_I_YAW_MAX) {
+          pid_i_yaw = PID_I_YAW_MAX;      
+      }else if(pid_i_yaw < PID_I_YAW_MAX * -1){
+          pid_i_yaw = PID_I_YAW_MAX * -1;
       }
     }else{
       pid_i_yaw = 0;
@@ -581,10 +685,10 @@ void setAllEnginesSpeed(int speed){
     pid_d_yaw = (yaw_error - yaw_error_previous) * PID_D_GAIN_YAW;
 
     pid_yaw_output = pid_p_yaw + pid_i_yaw + pid_d_yaw;
-    if (pid_yaw_output > pid_max) {
-        pid_yaw_output = pid_max;      
-    }else if(pid_yaw_output < pid_max * -1){
-        pid_yaw_output = pid_max * -1;    
+    if (pid_yaw_output > PID_YAW_MAX) {
+        pid_yaw_output = PID_YAW_MAX;      
+    }else if(pid_yaw_output < PID_YAW_MAX * -1){
+        pid_yaw_output = PID_YAW_MAX * -1;    
     }
     yaw_error_previous = yaw_error;
   }
@@ -593,6 +697,15 @@ void setAllEnginesSpeed(int speed){
   rightRear = speed +  ((-pid_pitch_output - pid_roll_output + pid_yaw_output));
   leftFront= speed +   ((pid_pitch_output + pid_roll_output + pid_yaw_output));
   rightFront = speed + ((pid_pitch_output - pid_roll_output - pid_yaw_output));
+
+  //Battery drop compensation
+  if (batteryVin < 12.4f && batteryVin > 8.0f) {
+    float compensation = (12.4f - batteryVin) / 35.0f;
+    leftRear   += leftRear * compensation;
+    rightRear  += rightRear * compensation;
+    leftFront  += leftFront * compensation;
+    rightFront += rightFront * compensation;
+  }
 
   if(leftRear > SPEED_MAX_OUTPUT){
     leftRear = SPEED_MAX_OUTPUT;
@@ -634,33 +747,48 @@ void setAllEnginesSpeed(int speed){
   }
 
   //Rear engines
-  esc1.writeMicroseconds(leftRear);//Left 
-  esc2.writeMicroseconds(rightRear);//Right
+  // esc1.writeMicroseconds(leftRear);//Left 
+  // esc2.writeMicroseconds(rightRear);//Right
+  esc1->setPWM(pinESC1,250.0f,leftRear/40.0f);
+  esc2->setPWM(pinESC2,250.0f,rightRear/40.0f);
+
 
   //Front engines
-  esc3.writeMicroseconds(leftFront);//Left  
-  esc4.writeMicroseconds(rightFront);//Right
+  // esc3.writeMicroseconds(leftFront);//Left  
+  // esc4.writeMicroseconds(rightFront);//Right
+  esc3->setPWM(pinESC3,250.0f,leftFront/40.0f);
+  esc4->setPWM(pinESC4,250.0f,rightFront/40.0f);
 
   #ifdef PRINT_PARAMETERS
-    buf += String(leftRear);
-    buf += F(",");
-
-    buf += String(rightRear);
-    buf += F(",");
-
-    buf += String(leftFront);
-    buf += F(",");
-      
-    buf += String(rightFront);
-    buf += F(",");  
-
-    buf += String(pid_i_pitch);
-    buf += F(",");
     
-    buf += String(pid_i_roll);
-    buf += F(",");  
+    flightLog.i_pitch = pid_i_pitch;
+    flightLog.i_roll = pid_i_roll;
+    flightLog.i_yaw = pid_i_yaw;
+
+    flightLog.leftRear = leftRear;
+    flightLog.rightRear = rightRear;
+    flightLog.leftFront = leftFront;
+    flightLog.rightFront = rightFront;
+    
+    // buf += String(leftRear);
+    // buf += F(",");
+
+    // buf += String(rightRear);
+    // buf += F(",");
+
+    // buf += String(leftFront);
+    // buf += F(",");
       
-    buf += String(pid_i_yaw);
+    // buf += String(rightFront);
+    // buf += F(",");  
+
+    // buf += String(pid_i_pitch);
+    // buf += F(",");
+    
+    // buf += String(pid_i_roll);
+    // buf += F(",");  
+      
+    // buf += String(pid_i_yaw);
   #endif
    
 }
@@ -821,5 +949,15 @@ void getMotion(int16_t* ax, int16_t* ay, int16_t* az, int16_t* gx, int16_t* gy, 
   #endif
 }
 
+void readBatteryVoltage(){
+  // Read analog value and smooth
+  batteryADCRaw = analogRead(pinBattery);
+  batteryADC = (batteryADC * 0.92) + ((float)batteryADCRaw * 0.08);
+  
+  // Convert it analog PIN volts
+  batteryVinRaw = (batteryADC / 4095.0) * REFERENCE_VOLTAGE;
 
+  // Calculate the battery volts
+  batteryVin = batteryVinRaw * ( BATTERY_R1 + BATTERY_R2 ) / BATTERY_R2;
 
+}
